42a636ef99d4eca5c1eb0764f8194bb1e1105f7a
Yuriy Kaplun <ykaplun@malkosua.com>
2015-03-25 11:34:53 +0200

    Added code to console application

--- Client (Console)/Program.cs
+++ Client (Console)/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Client__Console_
+﻿using System;
+using System.Linq;
+using Data.Repositories;
+
+namespace Client__Console_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var categoryRepository = new CategoryRepository();
+
+            var categories = categoryRepository.All().ToList();
+
+            foreach (var category in categories)
+            {
+                Console.WriteLine(category.Name);
+            }
+
+            Console.ReadLine();
         }
     }
 }
